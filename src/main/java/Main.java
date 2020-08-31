91d56b798da07d83b4d4f84964ea1b894f270bd8
Jakob Waibel <jakob.waibel@web.de>
2020-08-31 13:37:46 +0200

    Added the ability to handle multiple users with HashMap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,40 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        HashMap<String, String> dict = new HashMap<>();
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter username: ");
         String username = scan.nextLine();
         System.out.println("Enter password: ");
         String password = scan.nextLine();
-        Main check = new Main();
-        if (check.is_valid_credentials(username, password)) {
-            System.out.println("Seriously??? You chose password123 as your password?");
 
+        dict.put("robert", "password123");
+        dict.put("Jakob", "password123");
+        System.out.println(dict.get("robert"));
+
+
+        Main check = new Main();
+
+        if (check.is_valid_credentials(dict, username, password)) {
+            System.out.println("Seriously??? You chose password123 as your password?");
         } else {
             System.out.println("GET LOST!");
-
         }
 
     }
 
-    public boolean is_valid_credentials(String username, String password) {
-        if (username.equals("robert") && password.equals("password123")) {
-            return true;
+    public boolean is_valid_credentials(HashMap<String, String> dict, String username, String password) {
+
+        for (String i : dict.keySet()) {
+            if (i.equals(username) && dict.get(username).equals(password)) {
+                return true;
+            }
         }
-        else {
-            return false;
-        }
+        return false;
+
     }
 
 }
